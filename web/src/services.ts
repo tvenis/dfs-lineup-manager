07589c8e33b8dd73df3c2caf34c2616c35d479e1
Troy Venis <tvenis@gmail.com>
2025-08-27 17:18:22 -0400

    fix: remove unused services.ts file that was causing module resolution errors

--- web/src/services.ts
+++
@@ -1,7 +0,0 @@
-// Barrel export file to help with module resolution issues
-export { PlayerService } from './lib/playerService';
-export { LineupService } from './lib/lineupService';
-export type { PlayerPoolResponse, Week, WeekFilters } from './lib/playerService';
-export type { LineupCreate } from './lib/lineupService';
-export type { PlayerPoolEntry, Player, LineupSlotId, Lineup } from './types/prd';
-export { useLineupsStore } from './store';
